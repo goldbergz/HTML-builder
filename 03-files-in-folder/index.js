fd083e9af5daa8f2a3240b2a925a5f5da749205e
goldbergz <zolotogornaya22@mail.ru>
2025-01-17 17:19:05 +0300

    feat: do the 3 task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const dirPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  files.forEach(file => {
+    if (file.isFile()) {
+      const fullPath = path.join(dirPath, file.name);
+
+      fs.stat(fullPath, (err, stats) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        const fileName = path.parse(file.name).name;
+        const fileExt = path.parse(file.name).ext.slice(1);
+        const fileSizeKB = stats.size / 1024;
+        console.log(`${fileName} - ${fileExt} - ${fileSizeKB}kb`);
+      })
+    }
+  })
+})
